aef65479be80ef460fffcd9addd9d1c599f37fe3
Andhika Satya Prawira <ndhik@Andhikas-MacBook-Pro.local>
2023-09-30 23:25:21 +0700

    Fix fonts

--- app/components/fonts.js
+++ app/components/fonts.js
@@ -8,24 +8,22 @@ const Fonts = () => (
         font-style: normal;
         font-weight: 500;
         font-display: swap;
-        src: url('/fonts/PixelifySans-Regular.ttf') format('ttf');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        src: url('/fonts/pixelify_sans/PixelifySans-Regular.ttf') format('truetype');
 }
 @font-face {
   font-family: 'Pixelify Sans';
         font-style: bold;
         font-weight: 700;
         font-display: swap;
-        src: url('/fonts/PixelifySans-Bold.ttf') format('ttf');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        src: url('/fonts/pixelify_sans/PixelifySans-Bold.ttf') format('truetype');
+        
 }
 @font-face {
-  font-family: 'Pixelify Sans';
+  font-family: 'Ligo';
         font-style: normal;
         font-weight: 500;
         font-display: swap;
-        src: url('/fonts/Ligo.ttf') format('ttf');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+        src: url('/fonts/ligo/Ligo.ttf') format('truetype');
 }`}
   />
 );
